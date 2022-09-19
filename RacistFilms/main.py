5fdab128c8c6c79020da387c545faa37449d5959
kretgwiazdonosy <64355931+kretgwiazdonosy@users.noreply.github.com>
2022-09-19 12:37:23 +0200

    Add files via upload

--- RacistFilms/main.py
+++ RacistFilms/main.py
@@ -3,48 +3,13 @@ import glob
 import cv2
 import keyboard
 
-# create paths to films
-print('Tworzenie ścieżek')
-films_path = []
-for film_path in glob.glob('materials/videos/*.mp4'):
-    films_path.append(film_path.replace(chr(92), '/'))
 
-races = {'black': 0,
-         'white': 0,
-         'asian': 0,
-         'indian': 0,
-         'middle eastern': 0,
-         'latino hispanic': 0}
+def main():
 
-# read those videos
-print('Odtworzenie filmów')
-for film_path in films_path:
-    vid_name = film_path.split('/')[-1]
-    vid = cv2.VideoCapture(film_path)
-
-    isVidPlaying, frame = raceutils.read_video(vid, frames_skipped=0)
-
-    print('Wykonanie pętli isVidPlaying')
-    while isVidPlaying:
-
-        for face_area in raceutils.detect_face_areas(frame):
-            detection = raceutils.race_detect(face_area)
-
-            print(detection)
-            for race in races:
-                if race == detection['dominant_race']:
-                    races[race] += 1
-                    break
-
-            print(f'Przeanalizowano {int((vid.get(cv2.CAP_PROP_POS_FRAMES)/vid.get(cv2.CAP_PROP_FRAME_COUNT)*100))}%'
-                  f' filmu {vid_name}')
-
-        isVidPlaying, frame = raceutils.read_video(vid, frames_skipped=45)
-
-        if keyboard.is_pressed('q'):
-            break
-
-    raceutils.build_figure(races, vid_name).write_image(f"graphs/{vid_name.replace('.mp4', '')}.pdf")
+    print('Creating paths to films')
+    films_path = []
+    for film_path in glob.glob('materials/videos/*.mp4'):
+        films_path.append(film_path.replace(chr(92), '/'))
 
     races = {'black': 0,
              'white': 0,
@@ -53,4 +18,45 @@ for film_path in films_path:
              'middle eastern': 0,
              'latino hispanic': 0}
 
-vid.release()
+    # read those videos
+    print('Films added to analysis queue')
+    for film_path in films_path:
+        vid_name = film_path.split('/')[-1]
+        vid = cv2.VideoCapture(film_path)
+
+        isVidPlaying, frame = raceutils.read_video(vid, frames_skipped=0)
+
+        print('Film analysis has been started')
+        while isVidPlaying:
+
+            for face_area in raceutils.detect_face_areas(frame):
+                detection = raceutils.race_detect(face_area)
+
+                print(detection)
+                for race in races:
+                    if race == detection['dominant_race']:
+                        races[race] += 1
+                        break
+
+                print(f'{int((vid.get(cv2.CAP_PROP_POS_FRAMES)/vid.get(cv2.CAP_PROP_FRAME_COUNT)*100))}% of {vid_name}'
+                      f' video has been analyzed')
+
+            isVidPlaying, frame = raceutils.read_video(vid, frames_skipped=45)
+
+            if keyboard.is_pressed('q'):
+                break
+
+        raceutils.build_figure(races, vid_name).write_image(f"graphs/{vid_name.replace('.mp4', '')}.pdf")
+
+        races = {'black': 0,
+                 'white': 0,
+                 'asian': 0,
+                 'indian': 0,
+                 'middle eastern': 0,
+                 'latino hispanic': 0}
+
+        vid.release()
+
+
+if __name__ == '__main__':
+    main()
